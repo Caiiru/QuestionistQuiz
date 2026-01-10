64a151ba9e53c047739b1c6800b02be889d1c705
caiiru <caiogome125@gmail.com>
2026-01-10 19:03:52 -0300

    Project Dependencies Update

--- U_Questionist/Assets/_Project/Scripts/Lobby/LobbyManager.cs
+++ U_Questionist/Assets/_Project/Scripts/Lobby/LobbyManager.cs
@@ -1,16 +1,39 @@
+using Unity.Services.Core;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
+using Unity.Services.Authentication;
+using Unity.Services.Lobbies;
+using AtomicConsole;
 
 public class LobbyManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Lobby _lobby;
+
+    private async void Start()
     {
+        await UnityServices.InitializeAsync();
         
+        
+        
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
-    // Update is called once per frame
-    void Update()
+    [AtomicCommand(name:"CreateLobby")]
+    private async void CreateLobby()
     {
-        
+
+        try
+        {
+            string lobbyName = "New Lobby";
+            int maxPlayers = 4;
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
+
+            Debug.Log("Created Lobby");
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.LogException(e);
+        }
     }
+
 }
